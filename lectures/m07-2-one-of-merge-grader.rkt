19873e21267142d19d9aaf2aea81c2bd3b6e5541
Gregor Kiczales <gregork@students.cs.ubc.ca>
2023-11-10 19:29:57 -0800

    l17

--- lectures/m07-2-one-of-merge-grader.rkt
+++ lectures/m07-2-one-of-merge-grader.rkt
@@ -20,7 +20,7 @@
 
       (grade-problem 1
         (grade-htdf merge            
-          (weights (.05   .1 .1 .05   .1 *)
+          (weights (.05   .15   .15 .05  .30  0 *)
                      
             (grade-signature (LON LON -> LON))
 
@@ -28,6 +28,7 @@
               (and (list? lon1) (andmap number? lon1))
               (and (list? lon2) (andmap number? lon2))
               (equal? r (%%merge lon1 lon2)))
+
             (grade-argument-thoroughness
               (per-args (lon1 lon2)
                 (and (empty? lon1) (empty? lon2))
@@ -36,6 +37,7 @@
                 (and (not (empty? lon2)) (not (empty? lon1)))
                 (>= (length lon1) 2)
                 (>= (length lon2) 2)))
+
             (grade-thoroughness-by-faulty-functions 1
               (define (merge lon1 lon2)
                 (local [(define (merge l1 l2)
@@ -98,6 +100,7 @@
                                (merge l1 (rest l2))))]))              
             
             (grade-submitted-tests 1)
+            
             (grade-additional-tests 1
               (check-expect (merge empty empty) empty)
               (check-expect (merge empty (list 2 3 5)) (list 2 3 5))
