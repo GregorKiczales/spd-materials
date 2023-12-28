d7bdc1b0930c4d0e8fd42aba8c0ad75876156d00
Gregor Kiczales <gregork@students.cs.ubc.ca>
2023-12-27 21:12:35 -0800

    use new form of grade-top-level-expression

--- lectures/m01-primitives-grader.rkt
+++ lectures/m01-primitives-grader.rkt
@@ -9,26 +9,16 @@
   (lambda ()
     (grade-submission
 
-      (define %%P2-SOLUTION
-        (beside (rectangle 100 200 "solid" "red")
-                (rectangle 200 200 "solid" "white")
-                (rectangle 100 200 "solid" "red")))
-
       (weights (*)
+
         (grade-problem 1
-          (let* ([sexps (problem-sexps (car (context)))]
-                 [corr? (and (= (length sexps) 1)
-                             (member (car sexps) '((sqrt (+ (sqr 3)    (sqr 4)))
-                                                   (sqrt (+ (* 3 3)    (* 4 4)))
-                                                   (sqrt (+ (expt 3 2) (expt 4 2))))))])
-            (rubric-item 'other
-                         corr?
-                         "Expression that clearly computes  sqrt ( 3^2 + 4^2 )")))
+          (score-max
+           (grade-top-level-expression () (sqrt (+ (sqr 3)    (sqr 4))))
+           (grade-top-level-expression () (sqrt (+ (* 3 3)    (* 4 4))))
+           (grade-top-level-expression () (sqrt (+ (expt 3 2) (expt 4 2))))))
+        
         (grade-problem 2
-          (let* ([sexps  (problem-sexps (car (context)))]
-                 [called (called-fn-names (car sexps))])
-            (weights (*)
-              (rubric-item 'template (member 'rectangle called) "Calls rectangle")
-              (rubric-item 'template (= (length (filter (lambda (x) (eq? x 'rectangle)) called)) 3) "Calls rectangle 3 times")
-              (rubric-item 'template (member 'beside called) "Calls beside")
-              (rubric-item 'other (calling-evaluator #f `(equal? ,(car sexps) %%P2-SOLUTION)) "Produces correct image"))))))))
+          (grade-top-level-expression ()
+            (beside (rectangle 100 200 "solid" "red")
+                    (rectangle 200 200 "solid" "white")
+                    (rectangle 100 200 "solid" "red"))))))))
