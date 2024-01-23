4535b6d914623c3fb43e01cc98656c6dc81c9259
GregorKiczales <38510095+GregorKiczales@users.noreply.github.com>
2024-01-22 18:43:42 -0800

    Update m10-reverse-grader.rkt

--- lectures/m10-reverse-grader.rkt
+++ lectures/m10-reverse-grader.rkt
@@ -10,9 +10,7 @@
       (grade-problem 1
         (grade-htdf rev
           (let* ([sexps  (map elt-sexp (elts))]
-                 [htdf   (car (context))]
-                 [defn   (htdf-defns htdf)]
-                 [local-defines (cdr (defines defn))]
+                 [local-defines (cdr (defines rev))]
                  [fn-for-lox (car local-defines)]
                  [params (cadr fn-for-lox)]
                  [cond-exp (caddr fn-for-lox)]
@@ -20,17 +18,18 @@
                                                 (symbol? (cadr (cadr  cond-exp)))            ;(cond [... rsf
                                                 (member  (cadr (cadr  cond-exp))
                                                          (remove 'lox params)))])
+            (ensure-unchanged SIG)
+            (ensure-unchanged TESTS)
             (weights (*)
-              (rubric-item 'signature (unchanged? REV-SIG-TESTS sexps)
-                           "the supplied signature and check-expects must not be changed")
               (rubric-item 'template produces-value-of-param?
-                           "has accumulator parameter and produce its result in base case")
+                           "Has accumulator parameter and produce its result in base case")
               (grade-tail-recursive))))))))
 
-(define REV-SIG-TESTS
-  '((@signature (listof X) -> (listof X))
-    ;; produce list of same elements in the opposite order
-    (check-expect (rev empty) empty)
+(define SIG
+  '((@signature (listof X) -> (listof X))))
+
+(define TESTS
+  '((check-expect (rev empty) empty)
     (check-expect (rev (list 1)) (list 1))
     (check-expect (rev (list "a" "b" "c")) (list "c" "b" "a"))))
 
