bdf891f8b45e43f1fd449efeb782eb05eab298da
GregorKiczales <38510095+GregorKiczales@users.noreply.github.com>
2024-12-17 09:36:44 -0800

    turn-off-show-sharing-in-values-in-37-files

--- bank/skipn-tr-order-solution.rkt
+++ bank/skipn-tr-order-solution.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname f-p6-solution) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname f-p6-solution) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require spd/tags)
 
 (@assignment bank/skipn-tr-order)
