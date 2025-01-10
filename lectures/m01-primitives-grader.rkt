5fc5d7457cbf2f2e46006bf3bcda0c53fa4a29f1
GregorKiczales <38510095+GregorKiczales@users.noreply.github.com>
2025-01-09 19:37:35 -0800

    use may-use-values argument to grade-top-level expression

--- lectures/m01-primitives-grader.rkt
+++ lectures/m01-primitives-grader.rkt
@@ -13,11 +13,11 @@
 
         (grade-problem 1
           (score-max
-           (grade-top-level-expression () (sqrt (+ (sqr 3)    (sqr 4))))
-           (grade-top-level-expression () (sqrt (+ (expt 3 2) (expt 4 2))))))
+           (grade-top-level-expression () (3 4)   (sqrt (+ (sqr 3)    (sqr 4))))
+           (grade-top-level-expression () (3 4 2) (sqrt (+ (expt 3 2) (expt 4 2))))))
         
         (grade-problem 2
-          (grade-top-level-expression ()
+          (grade-top-level-expression () *
             (beside (rectangle 100 200 "solid" "red")
                     (rectangle 200 200 "solid" "white")
                     (rectangle 100 200 "solid" "red"))))))))
